be62ce721f0d9fdeaade45fe49d710594b17ab82
Mex777 <fologeaalexandru@gmail.com>
2020-02-15 23:25:24 +0200

    variables

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int x = (12+15) * (2+3);
+        int y = x+5;
+        int z = x+y;
+        System.out.println(x + " " + y + " " + z);
     }
 }
