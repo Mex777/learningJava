c183c7781e54c9d855407baeb6e9ca21d5e77c3d
Mex777 <fologeaalexandru@gmail.com>
2020-02-16 10:33:37 +0200

    Problem 3 solved: minutes and seconds

---
+++ src/minutesAndSeconds.java
@@ -0,0 +1,30 @@
+public class minutesAndSeconds {
+	public static void main(String[] args) {
+		System.out.println(getDurationString(80, 40));
+		System.out.println(getDurationString(3800));
+	}
+	public static String getDurationString(int minutes, int seconds) {
+		if (minutes < 0 || seconds < 0 || seconds >= 60)
+			return "Invalid value";
+		else { int XX=0, YY, ZZ = seconds;
+			while (minutes>59) {
+				XX++;
+				minutes -= 60;
+			}
+			YY = minutes;
+			String value = "" + XX +"h " + YY + "m " + ZZ + "s ";
+			return value;
+		}
+	}
+	public static String getDurationString(int seconds) {
+		if (seconds < 0)
+			return "Invalid value";
+		else { int minutes = 0;
+			while (seconds >= 60) {
+				minutes++;
+				seconds -= 60;
+			}
+			return getDurationString(minutes, seconds);
+		}
+	}
+}
