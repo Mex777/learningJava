07a7fbe8212b143396286fb4f8690c27d3bb4134
Mex777 <fologeaalexandru@gmail.com>
2020-02-16 09:58:15 +0200

    Prob 2 improved

--- src/prob2.java
+++ src/prob2.java
@@ -1,23 +1,42 @@
+class Concurent {
+	private String name;
+	private int score;
+	Concurent (String aName, int aScore) {
+		name = aName;
+		score = aScore;
+	}
+
+	public void setScore (int scor) {
+		score = scor;
+	}
+	public void setName (String sN) {
+		name = sN;
+	}
+	public int getScore() {
+		return score;
+	}
+	public String getName() {
+		return name;
+	}
+}
 public class prob2 {
 
-    public static void main(String[] args) { prob2 go = new prob2();
-        int[] score = new int[4];
-        score[0] = 1500; score[1] = 400; score[2] = 900; score[3] = 50;
-        String[] participanti = new String[4];
-        participanti[0] = "Gelu";
-        participanti[1] = "Marinel";
-        participanti[2] = "George";
-        participanti[3] = "Cristi";
-        for (int i=0; i<4; i++) { 
-            int position = go.calculatePosition(score[i]);
-            go.displayHighScorePosition(participanti[i], position);
-        }
+    public static void main(String[] args) { Concurent[] participant = new Concurent[4];
+
+ 		participant[0] = new Concurent("Gigel", 1500);
+ 		participant[1] = new Concurent("Marius", 900);
+ 		participant[2] = new Concurent("Cristi", 50);
+ 		participant[3] = new Concurent("George", 400);
+ 		for (int i = 0; i<4; i++) {
+ 			int position = calculatePosition(participant[i].getScore());
+ 			displayHighScorePosition (participant[i].getName(), position);
+ 		}
         
     }
-    public void displayHighScorePosition(String playerName, int position) {
+    public static void displayHighScorePosition(String playerName, int position) {
         System.out.println(playerName + " managed to get into position " + position);
     }
-    public int calculatePosition(int score) { int position;
+    public static int calculatePosition(int score) { int position;
         if (score > 1000)
             return 1;
         else if (score > 500 && score < 1000)
