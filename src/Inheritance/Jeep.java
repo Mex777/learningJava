f968ea846bb5ad297fb753cd5c2226abcdcc3b54
Mex777 <fologeaalexandru@gmail.com>
2020-02-16 13:47:18 +0200

    Inheritance exercise complete

---
+++ src/Inheritance/Jeep.java
@@ -0,0 +1,19 @@
+import java.sql.SQLOutput;
+
+public class Jeep extends Car {
+    String model = "Jeep";
+    String speed = "Fast";
+    private String type;
+
+    public void setType (String type) {
+        this.type = type;
+    }
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public void sound() {
+        System.out.println("Vroom, Vroom!");
+    }
+}
---
+++ src/Inheritance/Vehicle.java
@@ -0,0 +1,9 @@
+
+public abstract class Vehicle {
+     private   String typeOfVehicle;
+     private  String model;
+     private  String speed;
+
+
+    abstract public void sound() ;
+}
